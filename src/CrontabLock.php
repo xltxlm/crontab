7ce095dfab235b54c14ea0b598fa820bfe6430fe
夏琳泰 <xltxlm@qq.com>
2017-01-09 19:16:45 +0800

    优化代码

--- src/CrontabLock.php
+++ src/CrontabLock.php
@@ -66,7 +66,7 @@ trait CrontabLock
             } else {
                 if ($pid) {
                     //父进程会得到子进程号，所以这里是父进程执行的逻辑
-                    $this->log('父进程:进入父进程id:'.(int) posix_getpid());
+                    $this->log('父进程:进入父进程id:'.(int)posix_getpid());
                     $this->log("父进程:进入父进程,监听进程id:$pid");
                     $res = pcntl_wait($status);
                     if ($res == -1 || $res > 0) {
@@ -79,8 +79,8 @@ trait CrontabLock
                     if ($this->childNum > $this->childMaxNum) {
                         exit;
                     }
-                    $this->childPid = (int) posix_getpid();
-                    $this->log('子进程:开始运行,进程id:'.(int) posix_getpid());
+                    $this->childPid = (int)posix_getpid();
+                    $this->log('子进程:开始运行,进程id:'.(int)posix_getpid());
                     //子进程得到的$pid为0, 所以这里是子进程执行的逻辑。
                     while (true) {
                         $this->log('子进程:真实代码开始运行');
@@ -106,7 +106,7 @@ trait CrontabLock
             throw new \Exception('无法打开文件,文件加锁失败,是不是已经存在启动进程?.'.$this->lockFile);
         }
         if (!$this->lock = flock($this->fp, LOCK_EX | LOCK_NB)) {
-            throw new \Exception('文件:'.$this->lockFile.'加锁失败,是不是已经存在启动进程? id:'.(int) posix_getpid());
+            throw new \Exception('文件:'.$this->lockFile.'加锁失败,是不是已经存在启动进程? id:'.(int)posix_getpid());
         }
         register_tick_function([$this, 'tick']);
     }
@@ -116,10 +116,10 @@ trait CrontabLock
      */
     public function __destruct()
     {
-        if ((int) posix_getpid() == $this->childPid) {
+        if ((int)posix_getpid() == $this->childPid) {
             return;
         }
-        $this->log('析构函数被调用:进程id:'.(int) posix_getpid());
+        $this->log('析构函数被调用:进程id:'.(int)posix_getpid());
         flock($this->fp, LOCK_UN);
         fclose($this->fp);
     }
@@ -129,6 +129,6 @@ trait CrontabLock
         if (!is_file($this->lockFile)) {
             throw new \Exception('锁文件被破坏!'.static::class);
         }
-        $this->log(date('Y-m-d H:i:s'));
+        //$this->log(date('Y-m-d H:i:s'));
     }
 }
